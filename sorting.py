d7521e4b873ddae1082326b09fce2a8b66c3a5f8
almirPGU <almirqhr@gmail.com>
2026-01-03 13:20:38 +0500

    улучшение алгоритма сортировки и добавление комментариев

--- sorting.py
+++ sorting.py
@@ -1,12 +1,21 @@
 # сортировка вставками
 
 def insertion_sort(arr, key):
-    temp = arr[:]
-    for i in range(1, len(temp)):
-        x = temp[i]
+    res = arr[:]  # копия списка, чтобы не менять исходный
+
+    for i in range(1, len(res)):
+        # сохраняем текущий элемент
+        new_elem = res[i]
+
+        # начинаем сравнение с предыдущего элемента
         j = i - 1
-        while j >= 0 and key(temp[j]) > key(x):
-            temp[j + 1] = temp[j]
+
+        # сдвигаем элементы, которые больше new_elem
+        while j >= 0 and key(res[j]) > key(new_elem):
+            res[j + 1] = res[j]
             j -= 1
-        temp[j + 1] = x
-    return temp
+
+        # вставляем элемент на нужное место
+        res[j + 1] = new_elem
+
+    return res

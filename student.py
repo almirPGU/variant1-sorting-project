4c9e77eafb14dd4f25134c9ce4c55d5d730d9307
almirPGU <almirqhr@gmail.com>
2026-01-08 17:31:24 +0500

    Улучшение структуры данных ученика и вывода

--- student.py
+++ student.py
@@ -16,9 +16,14 @@ def print_students(students):
     print("-" * 70)
     print(f"{'Фамилия':<12}{'Имя':<10}{'Дата рождения':<15}{'Класс'}")
     print("-" * 70)
+
     for student in students:
-        # Формируем дату в формате ДД.ММ.ГГГГ
         date = f"{student['day']:02}.{student['month']:02}.{student['year']}"
-        # Вывод одной строки таблицы
-        print(f"{student['surname']:<12}{student['name']:<10}{date:<15}{student['grade_num']}{student['grade_letter']}")
+        print(
+            f"{student['surname']:<12}"
+            f"{student['name']:<10}"
+            f"{date:<15}"
+            f"{student['grade_num']}{student['grade_letter']}"
+        )
+
     print("-" * 70)

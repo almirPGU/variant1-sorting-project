daab3fa499c4c764d3ac90355e868046ebb2ddf9
almirPGU <almirqhr@gmail.com>
2026-01-08 14:37:57 +0500

    добавление комментариев

--- data_io.py
+++ data_io.py
@@ -1,6 +1,6 @@
 from student import create_student
 
-
+# Загружает список учеников из текстового файла
 def load_students(filename):
     students = []
     try:
@@ -13,18 +13,22 @@ def load_students(filename):
     for line in f:
         line_num += 1
         parts = line.strip().split(";")
+
+          # Проверяем корректность количества данных по заданному формату(не больше 7)
         if len(parts) != 7:
             print(f"Ошибка формата в строке {line_num}")
             return None
         try:
+            # Преобразуем числовые поля
             day = int(parts[2])
             month = int(parts[3])
             year = int(parts[4])
             grade = int(parts[5])
         except ValueError:
+            # Если преобразование в число не удалось
             print(f"Ошибка чисел в строке {line_num}")
             return None
-
+        # Проверяем диапазоны допустимых значений
         if not (1 <= day <= 31 and 1 <= month <= 12 and 8 <= grade <= 11):
             print(f"Некорректные данные в строке {line_num}")
             return None

a87fad20ea23bb1f02f4503a583b3140fcf3070b
almirPGU <almirqhr@gmail.com>
2026-01-08 14:39:51 +0500

    добавление комментариев

--- student.py
+++ student.py
@@ -1,3 +1,4 @@
+# Создаёт и возвращает словарь с данными одного ученика
 def create_student(surname, name, day, month, year, grade_num, grade_letter):
     return {
         "surname": surname,
@@ -10,11 +11,14 @@ def create_student(surname, name, day, month, year, grade_num, grade_letter):
     }
 
 
+# Выводит список учеников в виде таблицы
 def print_students(students):
     print("-" * 70)
     print(f"{'Фамилия':<12}{'Имя':<10}{'Дата рождения':<15}{'Класс'}")
     print("-" * 70)
     for s in students:
+        # Формируем дату в формате ДД.ММ.ГГГГ
         date = f"{s['day']:02}.{s['month']:02}.{s['year']}"
+        # Вывод одной строки таблицы
         print(f"{s['surname']:<12}{s['name']:<10}{date:<15}{s['grade_num']}{s['grade_letter']}")
     print("-" * 70)

8f953cf06ea73bbad3cd37197b935a4b7d057461
almirPGU <almirqhr@gmail.com>
2026-01-08 17:13:38 +0500

    исправление

--- student.py
+++ student.py
@@ -16,9 +16,14 @@ def print_students(students):
     print("-" * 70)
     print(f"{'Фамилия':<12}{'Имя':<10}{'Дата рождения':<15}{'Класс'}")
     print("-" * 70)
-    for s in students:
-        # Формируем дату в формате ДД.ММ.ГГГГ
-        date = f"{s['day']:02}.{s['month']:02}.{s['year']}"
-        # Вывод одной строки таблицы
-        print(f"{s['surname']:<12}{s['name']:<10}{date:<15}{s['grade_num']}{s['grade_letter']}")
+
+    for student in students:
+        date = f"{student['day']:02}.{student['month']:02}.{student['year']}"
+        print(
+            f"{student['surname']:<12}"
+            f"{student['name']:<10}"
+            f"{date:<15}"
+            f"{student['grade_num']}{student['grade_letter']}"
+        )
+
     print("-" * 70)
